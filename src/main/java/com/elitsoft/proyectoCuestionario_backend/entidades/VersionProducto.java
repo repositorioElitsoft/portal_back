3bc34eea87b61a4e09a3bf4610c293fb0b71620c
Felipe <felipeadiazreyes@gmail.com>
2023-10-01 15:52:13 -0300

    product version entity

---
+++ src/main/java/com/elitsoft/proyectoCuestionario_backend/entidades/VersionProducto.java
@@ -0,0 +1,19 @@
+package com.elitsoft.proyectoCuestionario_backend.entidades;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "tbl_vrs_prd")
+public class VersionProducto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vrs_id;
+    private String vrs_name;
+    @ManyToOne
+    @JoinColumn(name = "prd_id")
+    private Long prd_id;
+}
