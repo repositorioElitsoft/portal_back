2ca134a28740bb4d1140c7a18b276ac05fa00d99
Rodrigo Salinas <ro.salinash@duocuc.cl>
2023-11-10 09:46:22 -0300

    Feture referencias academicas

--- src/main/java/com/elitsoft/proyectoCuestionario_backend/entidades/ReferenciaAcademica.java
+++ src/main/java/com/elitsoft/proyectoCuestionario_backend/entidades/ReferenciaAcademica.java
@@ -1,4 +1,29 @@
 package com.elitsoft.proyectoCuestionario_backend.entidades;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TBL_REF_ACAD")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data
 public class ReferenciaAcademica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ref_acad_id;
+
+    private String ref_acad_nom;
+    private String ref_acad_ins;
+    private String ref_acad_email;
+    private String ref_acad_tel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    @JoinColumn(name = "inf_acad_id")
+    private Academica academica;
 }
